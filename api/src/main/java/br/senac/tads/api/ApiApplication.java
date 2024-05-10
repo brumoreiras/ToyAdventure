88ac69dc5cf53c5188a54ca4e916dd7670f7c06e
Leonardo Silva <ldss1@outlook.com>
2024-05-10 20:56:11 -0300

    fix: remove PasswordEncoder

--- api/src/main/java/br/senac/tads/api/ApiApplication.java
+++ api/src/main/java/br/senac/tads/api/ApiApplication.java
@@ -2,20 +2,12 @@ package br.senac.tads.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 
 }
