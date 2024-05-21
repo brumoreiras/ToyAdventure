a1ce01b1372969f62491a34a297cdbbdc535b8db
Leonardo Silva <ldss1@outlook.com>
2024-05-20 22:19:33 -0300

    feat: Update CadastroUsuarioDTO to include permissao fiel

--- api/src/main/java/br/senac/tads/api/domain/usuario/CadastroUsuarioDTO.java
+++ api/src/main/java/br/senac/tads/api/domain/usuario/CadastroUsuarioDTO.java
@@ -1,7 +1,5 @@
 package br.senac.tads.api.domain.usuario;
 
-import br.senac.tads.api.entities.Permissao;
-
 public record CadastroUsuarioDTO(String nome, String cpf, String email, String senha, String confirmacaoSenha,
-		Permissao permissao, Boolean ativo) {
+		String permissao, Boolean ativo) {
 }
