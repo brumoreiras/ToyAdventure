d29066fe0941467dc88095c02d93e4cb189096be
Gabriel8bit <Gabriel.barbosa8@outlook.com>
2024-05-27 19:05:32 -0300

    Create PedidoController.java
    PedidoController

---
+++ api/src/main/java/br/senac/tads/api/controller/PedidoController.java
@@ -0,0 +1,55 @@
+package br.senac.tads.api.controller;
+
+import br.senac.tads.api.controller.PedidoController;
+import br.senac.tads.api.domain.pedido.Pedido;
+import br.senac.tads.api.domain.pedido.StatusPedido;
+import br.senac.tads.api.services.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @GetMapping
+    public ResponseEntity<List<Pedido>> listarPedidos() {
+        List<Pedido> pedidos = pedidoService.listarPedidos();
+        return new ResponseEntity<>(pedidos, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido) {
+        Pedido pedidoSalvo = pedidoService.criarPedido(pedido);
+        return new ResponseEntity<>(pedidoSalvo, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pedido> atualizarPedido(@PathVariable Long id, @RequestBody Pedido pedido) {
+        pedido.setId(id);
+        Pedido pedidoAtualizado = pedidoService.atualizarPedido(pedido);
+        return new ResponseEntity<>(pedidoAtualizado, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluirPedido(@PathVariable Long id) {
+        pedidoService.excluirPedido(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pedido> buscarPedidoPorId(@PathVariable Long id) {
+        Pedido pedido = pedidoService.buscarPedidoPorId(id);
+        if (pedido != null) {
+            return new ResponseEntity<>(pedido, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
