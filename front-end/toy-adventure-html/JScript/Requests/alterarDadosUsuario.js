92ff5c9696e2c461e8feada9289bf25755bc2c1d
Leonardo Silva <ldss1@outlook.com>
2024-06-03 19:30:04 -0300

    chore: Refactor alterarDadosUsuario

--- front-end/toy-adventure-html/JScript/Requests/alterarDadosUsuario.js
+++ front-end/toy-adventure-html/JScript/Requests/alterarDadosUsuario.js
@@ -4,7 +4,7 @@ const formulario = document.getElementById('formulario');
 
 if (formulario) {
     formulario.addEventListener('submit', async (evento) => {
-        
+
         /* evento.preventDefault(); */ // Evita o comportamento padrão de recarregar a página ao enviar o formulário
 
         console.log('Clicou no botão de salvar :::> ', user);
@@ -21,22 +21,24 @@ if (formulario) {
         // Obtém os valores dos campos do formulário
         const nome = formulario.querySelector('#nome').value;
         const senha = formulario.querySelector('#senha').value;
-        let grupo = ''; // Declarando a variável grupo aqui
+        let permissao = ''; // Declarando a variável grupo aqui
 
         // Obtém o grupo selecionado
         const grupoSelecionado = document.querySelector('input[name="permissao"]:checked');
 
         // Verifica se alguma opção foi selecionada
         if (grupoSelecionado) {
-            grupo = grupoSelecionado.value; // Atribui o valor do grupo selecionado
-            console.log('Grupo selecionado:', grupo);
+            permissao = grupoSelecionado.value; // Atribui o valor do grupo selecionado
+            console.log('Grupo selecionado:', permissao);
         } else {
             console.error('Nenhuma opção de grupo selecionada.');
             return; // Interrompe a execução da função
         }
 
+        debugger;
+
         // Envia uma requisição para atualizar os dados do usuário
-        await atualizarUsuario(id, nome, senha, grupo);
+        await atualizarUsuario(id, nome, senha, permissao);
 
         // Emite um alerta após o envio do formulário
         /* alert('Formulário enviado com sucesso!'); */
@@ -45,24 +47,24 @@ if (formulario) {
 
 
 // Função para enviar uma requisição para atualizar os dados do usuário
-async function atualizarUsuario(id, nome, senha, grupo) {
+async function atualizarUsuario(id, nome, senha, permissao) {
     console.log('ID do usuário:', id)
     console.log('ID do usuário:', nome)
     console.log('ID do usuário:', senha)
-    console.log('ID do usuário:', grupo)
+    console.log('ID do usuário:', permissao)
 
-   
+
     try {
         const token = localStorage.getItem('token');
         console.log('token :::> ', token)
-   
-        const response = await fetch(`http://localhost:3033/alterar-usuario?id=${id}`, {
+
+        const response = await fetch(`https://toyadventure.onrender.com/usuario/{id}`, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
             },
-            body: JSON.stringify({ nome, senha, grupo })
+            body: JSON.stringify({ nome, senha, permissao })
         });
 
         if (response.ok) {
