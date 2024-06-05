270a8a5d5febc2cb63ca25d97a3ed2b855aa4284
Gabriel8bit <Gabriel.barbosa8@outlook.com>
2024-06-05 17:12:11 -0300

    PedidosIntergracao

--- api/src/main/java/br/senac/tads/api/repository/PedidoRepository.java
+++ api/src/main/java/br/senac/tads/api/repository/PedidoRepository.java
@@ -1,9 +1,10 @@
 package br.senac.tads.api.repository;
 
+import br.senac.tads.api.repository.PedidoRepository;
+import br.senac.tads.api.domain.pedido.Pedido;
+// import br.senac.tads.api.domain.produto.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import br.senac.tads.api.entities.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
--- api/src/main/java/br/senac/tads/api/services/PedidoService.java
+++ api/src/main/java/br/senac/tads/api/services/PedidoService.java
@@ -1,16 +1,14 @@
 package br.senac.tads.api.services;
 
-//inserido opcionalmente apenas pra colocar a data de entrega 7 dias apos o pedido
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
-
+import br.senac.tads.api.domain.pedido.Pedido;
+import br.senac.tads.api.domain.pedido.StatusPedido;
+import br.senac.tads.api.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.senac.tads.api.domain.pedido.StatusPedido;
-import br.senac.tads.api.entities.Pedido;
-import br.senac.tads.api.repository.PedidoRepository;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @Service
 public class PedidoService {
@@ -23,28 +21,24 @@ public class PedidoService {
     }
 
     public Pedido criarPedido(Pedido pedido) {
-        // Definir a data do pedido como a data atual
         pedido.setDataPedido(LocalDate.now());
-
-        // Calcular a data de entrega como 7 dias após a data do pedido
         LocalDate dataEntrega = LocalDate.now().plus(7, ChronoUnit.DAYS);
         pedido.setDataEntrega(dataEntrega);
-
-        // Salvar o pedido no banco de dados
         return pedidoRepository.save(pedido);
     }
 
     public Pedido atualizarPedido(Pedido pedido) {
-        Pedido pedidoExistente = pedidoRepository.findById(pedido.getId()).orElse(null);
+        Pedido pedidoExistente = pedidoRepository.findById(pedido.getIdPedido()).orElse(null);
         if (pedidoExistente != null) {
             pedidoExistente.setDataPedido(pedido.getDataPedido());
             pedidoExistente.setValorTotal(pedido.getValorTotal());
             pedidoExistente.setStatus(pedido.getStatus());
             pedidoExistente.setCliente(pedido.getCliente());
             pedidoExistente.setObservacao(pedido.getObservacao());
+            pedidoExistente.setQuantidade(pedido.getQuantidade());
             return pedidoRepository.save(pedidoExistente);
         } else {
-            throw new RuntimeException("Pedido não encontrado com o ID: " + pedido.getId());
+            throw new RuntimeException("Pedido não encontrado com o ID: " + pedido.getIdPedido());
         }
     }
 
