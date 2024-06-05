4a7e9712284ea933736c2e6c0bec64baf02341a7
Leonardo Silva <ldss1@outlook.com>
2024-06-05 17:54:32 -0300

    chore: Remove unused WebConfig class and update PedidoController with CORS configuration

--- api/src/main/java/br/senac/tads/api/controller/PedidoController.java
+++ api/src/main/java/br/senac/tads/api/controller/PedidoController.java
@@ -1,16 +1,26 @@
 package br.senac.tads.api.controller;
 
-import br.senac.tads.api.domain.pedido.Pedido;
-import br.senac.tads.api.services.PedidoService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import br.senac.tads.api.domain.pedido.Pedido;
+import br.senac.tads.api.services.PedidoService;
 
 @RestController
 @RequestMapping("/pedidos")
+@CrossOrigin(origins = "*")
 public class PedidoController {
 
     @Autowired
